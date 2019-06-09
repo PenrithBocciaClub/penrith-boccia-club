54b3676c326cef16d8df3fccab0983543682a320
PenrithBocciaClub <51372654+PenrithBocciaClub@users.noreply.github.com>
2019-06-09 22:16:32 +1000

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,5 +1,36 @@
 <?php
-	if (isset($_POST["submit"])) {
+	* {box-sizing: border-box;}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+if (isset($_POST["submit"])) {
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
@@ -36,6 +67,5 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 	}
 }
 	}
-?>
 
 		
